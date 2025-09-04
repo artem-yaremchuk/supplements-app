b558a06f6dada5076f9ad2fc8053f8c4a6761f46
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-09-04 13:26:28 +0300

    feat(auth): add PrivateRoute and UserMenu components

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../redux/auth/selectors';
+import sprite from '../assets/images/icons.svg';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const user = useSelector(selectUser);
+
+  const handleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={handleOpen}
+        type="button"
+        className="text-btn-text flex items-center gap-2 rounded-full px-3 py-2"
+      >
+        <svg className="text-btn-text h-8 w-8 fill-current">
+          <use href={`${sprite}#icon-profile`}></use>
+        </svg>
+
+        {isOpen ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
+      </button>
+
+      {isOpen && (
+        <div className="bg-soft-bg absolute right-0 z-15 mt-2 w-56 rounded-md p-4 shadow-lg">
+          <p className="mb-1 text-sm font-semibold">{user?.name} Artem Yaremchuk</p>
+          <p className="text-sm text-gray-500">Free Account</p>
+          <hr className="mt-2 border-gray-400" />
+
+          <div className="mt-7 flex flex-col gap-1">
+            <h6 className="text-sm font-semibold">My Content</h6>
+            <Link to="/saved" className="text-sm text-gray-700 hover:underline">
+              Saved Pages
+            </Link>
+            <hr className="mt-2 border-gray-400" />
+          </div>
+
+          <div className="mt-7 flex flex-col items-start gap-1">
+            <h6 className="text-sm font-semibold">My Profile</h6>
+            <button type="button" className="text-sm text-gray-700 hover:underline">
+              Logout
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,11 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import UserMenu from '../UserMenu';
 
 const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <header className="border-b bg-white px-6 py-4 shadow-sm dark:bg-slate-800">
       <nav className="flex flex-col items-center gap-4 sm:flex-row sm:justify-between">
@@ -30,6 +35,8 @@ const Header = () => {
             </NavLink>
           </li>
         </ul>
+
+        {!isLoggedIn && <UserMenu />}
       </nav>
     </header>
   );
