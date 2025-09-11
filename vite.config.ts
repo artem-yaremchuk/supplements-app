83419809328b8894fbb9bd1ea9a752916693c8e9
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-09-11 16:38:26 +0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/supplements-app/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+}));
