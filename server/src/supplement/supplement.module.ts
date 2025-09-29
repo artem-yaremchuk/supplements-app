caa8686b753468dc927602328660bba75510470e
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-09-29 17:13:39 +0300

    fix(server): update tsconfig.build.json

--- server/src/supplement/supplement.module.ts
+++ server/src/supplement/supplement.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SupplementController } from './supplement.controller';
 import { SupplementService } from './supplement.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
