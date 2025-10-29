e930141ed413ec56e7197760cc2930ea01198f8c
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-10-29 19:50:13 +0200

    feat(docker, compose, prisma): add full docker compose setup with client, server, and local Postgres
    - implement automatic migrations and seed for local DB
    - move seed script into src for local build consistency

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -5,6 +5,6 @@ import type { PrismaConfig } from 'prisma';
 export default {
   schema: path.join(__dirname, 'prisma'),
   migrations: {
-    seed: 'ts-node prisma/seed.ts',
+    seed: 'ts-node src/prisma/seed.ts',
   },
 } satisfies PrismaConfig;
